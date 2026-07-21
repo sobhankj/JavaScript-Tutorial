3353860863c90f50023d44ed49b478795988edd4
Sobhan Kooshki <sobhankj.tehran@gmail.com>
2026-07-21 18:14:50 +0330

    session 3 use typeof , string , number , boolean

---
+++ session3 typeof/scripts/script.js
@@ -0,0 +1,7 @@
+const name = 'sobhan kooshki jahromi' // this is string
+const age = 20 // this is number
+const isStudent = true // this is boolean
+
+console.log("name is " + typeof name);
+console.log("age is " + typeof age);
+console.log("isStudent is " + typeof isStudent);
