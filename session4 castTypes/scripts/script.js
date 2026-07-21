07501220a31d2f9706d8f33cda24c5f131138b84
Sobhan Kooshki <sobhankj.tehran@gmail.com>
2026-07-21 18:28:59 +0330

    add session 4 we learn how to cast types together

---
+++ session4 castTypes/scripts/script.js
@@ -0,0 +1,16 @@
+let numStr = '10';
+let numNum = Number(numStr);
+console.log("numStr is " + typeof numStr);
+console.log("numNum is " + typeof numNum);
+
+
+let ageNum = 20;
+let ageStr = String(ageNum);
+console.log("ageNum is " + typeof ageNum);
+console.log("ageStr is " + typeof ageStr);
+
+
+let numberNum = 10;
+let numberBool = Boolean(numberNum);
+console.log("numberNum is " + typeof numberNum);
+console.log("numberBool is " + typeof numberBool);
